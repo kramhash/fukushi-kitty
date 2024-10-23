f953f8a5e353969a162655fb08f2c982a344e975
大橋 將史 ゲスト <mark@kramhash.net>
2024-10-23 19:58:18 +0900

    update

--- src/components/top/info.tsx
+++ src/components/top/info.tsx
@@ -11,8 +11,20 @@ export const Info = () => {
       className="rounded-t-[80px] bg-kitty_gray relative  w-full flex flex-col max-w-[1024px] mx-auto px-[2%]"
       style={{ y: -105 }}
     >
-      <Label className="mt-[96px] mx-auto">福祉のいろんな活躍の仕方</Label>
-      <motion.div className="mb-[70px]"></motion.div>
+      <Label className="mt-[96px] mx-auto mb-[2vw]">CASESTUDY</Label>
+      <motion.div className="relative flex justify-between items-end px-[3vw] w-full mb-[5%]">
+        <motion.div style={{ width: "13.7%" }}>
+          <motion.img src={prefix("assets/commons/kitty3.png")} alt="" />
+        </motion.div>
+        <motion.div style={{ x: "3%" }}>
+          <SVGTitle src={"assets/top/info/title-info.png"} width={578} />
+          <motion.p className="whitespace-pre-wrap font-black font-mplus1c text-16lg text-center mt-[5%]">{`福祉の仕事には、さまざまな活躍の仕方があります。
+あなたにあった働き方を選べます。`}</motion.p>
+        </motion.div>
+        <motion.div style={{ width: "17.3%", x: "2vw" }}>
+          <motion.img src={prefix("assets/commons/kitty4.png")} alt="" />
+        </motion.div>
+      </motion.div>
 
       <motion.div className="flex flex-col md:flex-row gap-[10px]">
         <InfoBox
@@ -23,6 +35,7 @@ export const Info = () => {
             "目の前の人に貢献したい人",
             "手に職をつけたい",
           ]}
+          iconWidth={149}
           titleWidth={350}
         ></InfoBox>
         <InfoBox
@@ -36,6 +49,7 @@ export const Info = () => {
           className="text-white"
           titleWidth={252}
           bgColor="bg-kitty_blue"
+          iconWidth={223}
         ></InfoBox>
       </motion.div>
       <motion.div className=" flex flex-col md:flex-row gap-[10px]">
@@ -49,6 +63,7 @@ export const Info = () => {
           className="text-white"
           titleWidth={210}
           bgColor="bg-kitty_red"
+          iconWidth={127}
         ></InfoBox>
         <InfoBox
           title="coordinator"
@@ -56,6 +71,7 @@ export const Info = () => {
           recommendTexts={["人と繋がるのが好き", "地域づくりに関心がある"]}
           className=""
           titleWidth={336}
+          iconWidth={187}
         ></InfoBox>
       </motion.div>
       <InfoInterview />
@@ -71,6 +87,7 @@ const InfoBox = ({
   recommendTexts,
   titleWidth,
   bgColor,
+  iconWidth,
 }: // color = "",
 {
   title: string;
@@ -81,18 +98,26 @@ const InfoBox = ({
   color?: string;
   titleWidth: number;
   bgColor?: string;
+  iconWidth: number;
 }) => {
   return (
     <Box className={`mb-[50px] ${className}`} bgColor={bgColor}>
-      <motion.div className="flex flex-col justify-start">
+      <motion.div className="flex flex-col justify-start h-full">
         <SVGTitle src={`assets/top/info/${title}.svg`} width={titleWidth} />
         {/* <motion.p className="font-notosans font-black text-[16px] mt-[50px]">
           {description}
         </motion.p> */}
-        <motion.div className="flex justify-center mt-[20px]">
+        <motion.div
+          className="flex justify-center mt-[20px] mx-auto grow items-center"
+          style={{
+            width: `clamp(${
+              iconWidth * 0.5
+            }px, 100vw * ${iconWidth} / 1024, ${iconWidth}px)`,
+          }}
+        >
           <motion.img
-            src={prefix("assets/top/heart.png")}
-            className="w-[30%]"
+            src={prefix(`assets/top/info/icon/${title}.png`)}
+            className="w-full h-auto"
           />
         </motion.div>
 

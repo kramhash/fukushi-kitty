32e429d2cbd4ad165d95e7a7cf382b06182b19b5
kramhash <mark@kramhash.net>
2024-11-29 12:40:41 +0900

    update

--- src/components/top/download.tsx
+++ src/components/top/download.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-import { Label, BlankIcon, SVGTitle, MiniArrow } from "../commons";
+import { Label, BlankIcon, MiniArrow } from "../commons";
 import Link from "next/link";
 import { prefix } from "@/utils";
 
